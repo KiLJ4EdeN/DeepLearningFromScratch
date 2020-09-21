949c887badc9e151ce3eabe3931dbee00d6c107f
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-09-21 07:47:23 +0330

    Update mxnet_linear_algebra.py

--- mxnet_essentials/mxnet_linear_algebra.py
+++ mxnet_essentials/mxnet_linear_algebra.py
@@ -2,40 +2,32 @@
 # !pip install mxnet gluoncv
 import mxnet as mx
 
-##########################
 # Instantiate two scalars
-##########################
 x = mx.nd.array([3.0]) 
 y = mx.nd.array([2.0])
 
-##########################
 # Add them
-##########################
 print('x + y = ', x + y)
 
-##########################
 # Multiply them
-##########################
 print('x * y = ', x * y)
 
-##########################
 # Divide x by y
-##########################
 print('x / y = ', x / y)
 
-##########################
 # Raise x to the power y. 
-##########################
 print('x ** y = ', mx.nd.power(x,y))
 
+# Convert Them to Scalars
 mx.nd.array([9]).asscalar()
 
-# Vectors
+# Create A Vector
 u = mx.nd.arange(4)
 print('u = ', u)
-
+# Access Elements.
 print(u[3])
 
+# Some Vector Operations.
 a = 2
 x = mx.nd.array([1,2,3])
 y = mx.nd.array([10,20,30])
@@ -43,20 +35,21 @@ print(a * x)
 print(a * x + y)
 
 # Matrices.
-
 A = mx.nd.zeros((5,4))
-A
+print(A)
 
+# Create a Matrice from a reshaped vector.
 x = mx.nd.arange(20)
 A = x.reshape((5, 4))
-A
+print(A)
 
+# Access elements.
 print('A[2, 3] = ', A[2, 3])
-
 print('row 2', A[2, :])
 print('column 3', A[:, 3])
 
-A.T
+# Transpose Operation.
+print(A.T)
 
 # Tensors
 X = mx.nd.arange(24).reshape((2, 3, 4))
