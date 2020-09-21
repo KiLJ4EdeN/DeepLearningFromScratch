5658f72243631d115ab8480c65c1ac7831e336be
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-09-21 07:51:25 +0330

    Update mxnet_nd.py

--- mxnet_essentials/mxnet_nd.py
+++ mxnet_essentials/mxnet_nd.py
@@ -2,26 +2,27 @@
 # !pip install mxnet gluoncv
 
 import mxnet as mx
-
+# Reporducible results.
 mx.random.seed(1)
 
 x = mx.nd.empty(shape=(3, 4))
-x
+print(x)
 
 x = mx.nd.zeros((3, 4))
-x
+print(x)
 
-# random normal dist with zero mean and one std.
+# random normal distribution sampled matrix with zero mean and one std.
 y = mx.nd.random_normal(0, 1, shape=(3, 4))
-y
+print(y)
 
+# shape and size.
 print(y.shape)
 print(y.size)
 
-x + y
+print(x+y)
 
+# exponential fn and matrix multiplication.
 mx.nd.exp(y)
-
 mx.nd.dot(x.T, y)
 
 # the memory place is allocated again for y.
@@ -36,21 +37,22 @@ print('id(y):', id(y))
 y[:] = x + y
 print('id(y):', id(y))
 
+# add without excessive memory allocation.
 mx.nd.elemwise_add(x, y, out=y)
 
 print('id(x):', id(x))
 x += y
-x
+print(x)
 print('id(x):', id(x))
 
 print(x[1:3])
 x[1,2] = 9.0
-x
+print(x)
 
 x[1:2,1:3]
 
 x[1:2,1:3] = 5.0
-x
+print(x)
 
 x = mx.nd.ones(shape=(3,3))
 print('x = ', x)
@@ -62,17 +64,17 @@ y = y.reshape((3,1))
 print('y = ', y)
 print('x + y = ', x+y)
 
+# Conversion to numpy arrays and back to mx.nd array.
 a = x.asnumpy()
 type(a)
 
 y = mx.nd.array(a) 
-y
+print(type(y))
 
 z = mx.nd.ones(shape=(3,3), ctx=mx.cpu(0))
-z
+print(z)
 
 # move to another device.
-
 x_gpu = x.copyto(mx.gpu(0))
 print(x_gpu)
 
@@ -80,7 +82,6 @@ print(z.context)
 
 # with blind copying more memory is occupied each time.
 # can be prevented with as_in_context.
-
 print('id(z):', id(z))
 z = z.copyto(mx.cpu(0))
 print('id(z):', id(z))
